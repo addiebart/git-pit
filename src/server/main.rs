0ccf984a6d7b2fe7b1101251f982edc18b93b8fc
3yassu <68251430+3yassu@users.noreply.github.com>
2025-10-18 16:44:26 -0500

    finally...

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,8 +1,10 @@
 use git2::Repository;
-use smol::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener, net::TcpStream};
+use smol::{io::{AsyncReadExt, Error, ErrorKind}, io::AsyncWriteExt, net::{TcpListener, TcpStream}};
 use async_tungstenite::accept_async;            
 use async_tungstenite::tungstenite::protocol::Message;  
 use smol::prelude::*;
+use http::{HeaderMap, Request, HeaderName, HeaderValue, request::Builder, Version, Method};
+use mews::{WebSocketContext, WebSocket, Connection};
 
 fn main() -> std::io::Result<()> {
     smol::block_on(async {
@@ -17,6 +19,79 @@ fn main() -> std::io::Result<()> {
 	})
 }
 
+async fn parse_http(stream: &mut TcpStream) -> std::io::Result<Request<()>>{
+	let mut buffer: [u8; 2048] = [0u8; 2048];
+	let buffer_size: usize = stream.read(&mut buffer).await?;
+	//split buffer somehow
+	let request_text: String = std::str::from_utf8(&buffer).unwrap().to_string();
+	let mut lines = request_text.split("\r\n");
+	let request_line = lines.next().unwrap_or_default();
+	
+    let mut parts = request_line.split_whitespace();
+    let method_str = parts.next().unwrap_or("GET");
+    
+    let path = parts.next().unwrap_or("/");
+    let _version_str = parts.next().unwrap_or("HTTP/1.1");
+
+    let method = method_str.parse::<Method>().unwrap_or(Method::GET);
+    let version = if _version_str == "HTTP/1.0" { Version::HTTP_10 } else { Version::HTTP_11 };
+
+    let mut req: Builder = Request::builder()
+		.method(method)
+		.uri(path)
+		.version(version);
+
+	if let Some(req_map) = req.headers_mut(){//: Option<&mut HashMap<>>
+		//set up request
+		for line in lines{
+			if let Some((key, value)) = line.split_once(":") {
+				match (key.trim().parse::<HeaderName>(), value.trim().parse::<HeaderValue>()){
+					(Ok(name), Ok(value)) => {
+						req_map.insert(name, value);
+					},
+					_ => return Err(Error::new(ErrorKind::InvalidData, "Data could not be parsed into string")),
+				}
+			}
+		}
+	}else{
+		return Err(Error::new(ErrorKind::NotFound, "Builder had error!"));
+	}
+	req.body(()).map_err(|e| Error::new(ErrorKind::NotFound, "Builder error when trying to make request!"))
+}
+async fn handle_connection(mut stream: TcpStream, addr: std::net::SocketAddr) -> std::io::Result<()>{
+	let parser = parse_http(&mut stream).await?;
+	if let Some(key) = parser.headers().get("Sec-WebSocket-Key"){
+		match key.to_str(){
+			Ok(sec_key) =>{
+				let ctx = WebSocketContext::new(sec_key);
+				let (sign, ws) = ctx.on_upgrade(
+					move |mut conn: Connection<TcpStream>| async move {
+						while let Ok(Some(msg)) = conn.recv().await {
+							match msg {
+								mews::Message::Text(text) => {
+									println!("Recieved text from {addr}: {text}");
+								}
+								mews::Message::Binary(bin) => {
+									println!("Recieved binary from {addr}: {bin:?}");
+								}
+								mews::Message::Close(close) => {
+									println!("Client {addr} disconnected");
+									break;
+								}
+								_ => {}
+							}
+						}
+					}
+				);
+			}
+			Err(e) => eprintln!("Sec-WebSocket-Key string error!! handshake failed at {addr}: {e:?}"),
+		}
+	}else{
+		eprintln!("Sec-WebSocket-Key not found! handshake failed at {addr}");
+	}
+	Ok(())
+}
+/*
 async fn handle_connection(stream: TcpStream, addr: std::net::SocketAddr){
 	match accept_async(stream).await{
 		Ok(mut websocket) => {
@@ -51,3 +126,4 @@ async fn handle_connection(stream: TcpStream, addr: std::net::SocketAddr){
 		}	
 	}
 
+*/
