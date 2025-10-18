0991fc18430ef6e922b8979a401a193e64b7b112
3yassu <68251430+3yassu@users.noreply.github.com>
2025-10-17 23:15:01 -0500

    begin git.rs work

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,15 @@
-fn main(){
-	println!("Hello, World!");
+use smol::{io, net, prelude::*, Unblock};
+mod structs;
+
+fn main() -> io::Result<()> {
+    smol::block_on(async {
+        let mut stream = net::TcpStream::connect("127.0.0.1:8080").await?;
+        let req = b"GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n";
+        stream.write_all(req).await?;
+
+        let mut stdout = Unblock::new(std::io::stdout());
+        io::copy(stream, &mut stdout).await?;
+        Ok(())
+    })
 }
+
---
+++ src/server/structs/git.rs
@@ -0,0 +1,1 @@
+//git.rs (Facilitating git calls to backend using smol)
